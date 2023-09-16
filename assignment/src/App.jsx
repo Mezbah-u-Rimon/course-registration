c7fd42b6ae8a53b54d89af2fe94948e3812feba9
Muzbah Uddin <rimonmr444@gmail.com>
2023-09-16 15:25:40 +0600

    added some changes

--- assignment/src/App.jsx
+++ assignment/src/App.jsx
@@ -50,7 +50,7 @@ function App() {
       else if (totalRemaining < 0) {
         toast.success('Sorry Total hours below zero(0) are not acceptable ', {
           position: "top-center",
-          autoClose: 5000,
+          autoClose: 3000,
           hideProgressBar: false,
           closeOnClick: true,
           pauseOnHover: true,
